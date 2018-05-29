796449443ed73be4843543edcf70266862ae5837
Sean Prasertsit <sean@wulfcode.com>
2018-05-28 19:50:13 -0700

    :speech_balloon: change attribution line

--- bot/CommentBuilder.js
+++ bot/CommentBuilder.js
@@ -1,6 +1,6 @@
 const ATTRIBUTION = `----
 
-^^^Created ^^^by ^^^\\/u/sheeann. ^^^[Contribute.](https://github.com/wulfcode/fortnite_bot) ^^^Report ^^^[issues](https://github.com/wulfcode/fortnite_bot/issues). ^^^Powered ^^^by ^^^[FortniteTracker](https://fortnitetracker.com/) ^^^and ^^^[fnbr.co](https://fnbr.co/).`;
+^^^Created ^^^by ^^^\\/u/sheeann. ^^^[Usage.](https://www.reddit.com/r/Fortnite_Bot/wiki/index) ^^^[Contribute.](https://github.com/wulfcode/fortnite_bot) ^^^Report ^^^[issues](https://github.com/wulfcode/fortnite_bot/issues). ^^^Powered ^^^by ^^^[FortniteTracker](https://fortnitetracker.com/) ^^^and ^^^[fnbr.co](https://fnbr.co/).`;
 
 const buildRow = cells => cells.join(' | ');
 
